023de42e28fec89784e733a4ee2acf0165f8a230
Ubuntu <ubuntu@ip-172-31-20-176.us-west-1.compute.internal>
2017-12-25 02:28:44 +0000

    Latest

--- monitis/jobs/sample.rb
+++ monitis/jobs/sample.rb
@@ -1,13 +1,19 @@
 current_valuation = 0
 current_karma = 0
+nextgen=1
+currentgen=2
+dashboard=3
 
-SCHEDULER.every '2s' do
+SCHEDULER.every '3s' do
   last_valuation = current_valuation
   last_karma     = current_karma
   current_valuation = rand(100)
   current_karma     = rand(200000)
 
-  send_event('valuation', { current: current_valuation, last: last_valuation })
-  send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: rand(100) })
+  #send_event('valuation', { current: current_valuation, last: last_valuation })
+  #send_event('karma', { current: current_karma, last: last_karma })
+  #send_event('synergy',   { value: rand(100) })
+  send_event('nextgen_widget',   { value: nextgen})
+  send_event('currentgen_widget',   { value: currentgen})
+  send_event('gohome_widget',   { value: dashboard})
 end
--- monitis/widgets/text/text.coffee
+++ monitis/widgets/text/text.coffee
@@ -1,1 +1,13 @@
 class Dashing.Text extends Dashing.Widget
+	@accessor 'value', Dashing.AnimatedValue
+
+
+onData: (data) ->
+    if data.value == 1
+      $(@node).css('background-color', '#6699ff')
+    if data.value == 2
+      $(@node).css('background-color', '#ffaa00')
+    if data.value == 3
+      $(@node).css('background-color', '#ffb3e6')
+    if data.value == 4
+      $(@node).css('background-color', '#47BBB3')
