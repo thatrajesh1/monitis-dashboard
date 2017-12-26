3d8929d4c5157c7b3cf06a769cce56b1afba8222
Ubuntu <ubuntu@ip-172-31-20-176.us-west-1.compute.internal>
2017-12-26 05:23:41 +0000

    run.sh

---
+++ monitis/run.sh
@@ -0,0 +1,6 @@
+cd /home/ubuntu/git/Monitis/java
+java -cp build/jar/custom-monitis.jar CustomMonitorReport monitorID_NGCR
+java -cp build/jar/custom-monitis.jar CustomMonitorReport monitorID_oapi
+java -cp build/jar/custom-monitis.jar CustomMonitorReport monitorID_loconnect
+java -cp build/jar/custom-monitis.jar CustomMonitorReport monitorID_tpo_ep3
+java -cp build/jar/custom-monitis.jar CustomMonitorReport monitorID_tpo_ep9
